30e04cbd7a166e1e78f41efc45519d91aa07204b
Catarina <catarina.manzieri@gmail.com>
2019-03-12 02:06:56 -0300

    Criar aplicação em Python/django

---
+++ landing/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def mostrar_index(request):
+    return render(request, 'index.html')
+
+def mostrar_formulario(request):
+    return render(request, 'formulario.html')
