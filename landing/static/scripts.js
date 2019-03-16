70b6692abcc2787532d8519400107b2ae66291ad
guilherme-amarante <guilhermeamarante@hotmail.com>
2019-03-15 22:10:07 -0300

    menu burguer

---
+++ landing/static/scripts.js
@@ -0,0 +1,11 @@
+const menuHamburguer = document.querySelector('.menu-hamburguer')
+const navMenuHamburguer = document.querySelector('.nav-menu-hamburguer')
+
+const escondeMostra = () => {
+    menuHamburguer.classList.toggle('esconde-menu');
+}
+
+hamburguer.onclick = escondeMostra
+
+
+
